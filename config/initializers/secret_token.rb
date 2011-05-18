a11bb6c0ec95f51cdb556e25fbea1f99cae8f7a6
Julia <imaginejulia@gmail.com>
2011-05-18 22:45:28 +0300

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeliveryApp::Application.config.secret_token = '4d9f5d8b59215e5783a443ba5d5833b9590f1b42081115b587ce331d377cd2511b92afdf5abbc5e59299b093b93fccacc5b10354c88f2dfdd1f84541a6b19782'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DeliveryApp::Application.config.session_store :cookie_store, :key => '_delivery_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DeliveryApp::Application.config.session_store :active_record_store
